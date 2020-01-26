b8ac0bc58be44e16093cc6da30b8440cc0d32ad9
Tim Dilks <tdilks@gmail.com>
2020-01-26 17:28:27 +0000

    Next lot of changes

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -5,18 +5,27 @@
       <!-- book-item v-for="book in books" v-bind:bookie="book" v-bind:key="book"></book-item -->
       <book-item v-for="book in books" :book="book"></book-item>
     </ul>
+    <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
+<!-- Back in BookList.vue's script tag, import BookForm from ./BookForm and add the BookForm component to the components object.  -->
 <script>
   import BookItem from "./BookItem";
+  import BookForm from "./BookForm";
   export default {
     name: "BookList",
     components: {BookItem},
+    methods: {
+      appendBook (bookTitle, bookAuthor) {
+        this.books.push({ title: bookTitle, author: bookAuthor });
+      }
+    },
     data: function() {
       return {
         components: {
-          BookItem
+          BookItem,
+          BookForm
         },
         books: [
             {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
