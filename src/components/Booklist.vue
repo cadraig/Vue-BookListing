c839582a59a2ac682880604fd8d7f2f75084ad23
Tim Dilks <tdilks@gmail.com>
2020-01-26 07:34:54 +0000

    Initial checkin

---
+++ src/components/Booklist.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Booklist",
+    data: function() {
+      return {
+        title: "All Books"
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
